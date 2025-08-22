88a4904412fa733180ecc5090e96363dc7e13ec9
alex <alex@doors.dev>
2025-08-22 13:43:05 +0300

    auth check in catalog

--- common/utils.go
+++ common/utils.go
@@ -1,9 +1,29 @@
 package common
 
 import (
+	"context"
 	"github.com/derstruct/doors-tutorial/driver"
 	"github.com/doors-dev/doors"
 )
+
+type sessionKey struct{}
+
+func StoreSession(ctx context.Context, session *driver.Session) {
+	doors.InstanceSave(ctx, sessionKey{}, session)
+}
+
+func LoadSession(ctx context.Context) *driver.Session {
+	session, ok := doors.InstanceLoad(ctx, sessionKey{}).(*driver.Session)
+	if !ok {
+		return nil
+	}
+	return session
+}
+
+func IsAuthorized(ctx context.Context) bool {
+	return LoadSession(ctx) != nil
+}
+
 
 func GetSession(r doors.R) *driver.Session {
 	c, err := r.GetCookie("session")
